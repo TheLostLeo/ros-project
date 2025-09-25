318bea88d91e6b4ee6d79962fc954ffb6fc2b2e7
tll <tll@example.com>
2025-09-25 10:48:39 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/tll/catkin_ws/src
+# Build directory: /home/tll/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("esp")
+subdirs("core")
+subdirs("ai")
---
+++ build/esp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tll/catkin_ws/src/esp
+# Build directory: /home/tll/catkin_ws/build/esp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
