4d2ee844a1d9aefb7e6ca2ddf2b984cfa2778ac1
Joel Anto <thelostleo224@gmail.com>
2025-09-27 13:30:46 +0530

    Delete build directory

--- build/inventory_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tll/catkin_ws/src/inventory_robot
-# Build directory: /home/tll/catkin_ws/build/inventory_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
